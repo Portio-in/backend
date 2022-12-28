82ea99a3bbbb5a3e8beb91991241f0d3c7d5a48a
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-29 02:50:04 +0530

    all routes has been configured for authentication middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const DEBUG = process.env.DEBUG || false;
 
 // Express
 const express = require('express');
+const Middlware = require("./middleware");
 const app = express();
 // Config
 app.disable('x-powered-by')
@@ -14,5 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Auth Routes
 app.use('/auth', require('./routes/auth'));
+app.use('/profile', Middlware.auth, require('./routes/profile'));
+app.use('/link', Middlware.auth, require('./routes/link'));
+app.use('/project', Middlware.auth, require('./routes/project'));
+app.use('/education', Middlware.auth, require('./routes/education'));
+app.use('/experience', Middlware.auth, require('./routes/experience'));
+app.use('/certificate', Middlware.auth, require('./routes/certificate'));
+app.use('/achievement', Middlware.auth, require('./routes/achievement'));
 
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
