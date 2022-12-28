5bf0d4e6df44f482f11cb4a30a109b39712dec96
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-29 02:57:26 +0530

    sample file updated

---
+++ config-example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GOOGLE_OAUTH_CLIENT_ID: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com",
+    GITHUB_OAUTH_CLIENT_ID: "xxxxxxxxxxxxxxxxxxx",
+    GITHUB_OAUTH_CLIENT_SECRET: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+};
