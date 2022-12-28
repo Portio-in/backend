6f40e5161f68bb589ac5133cd55d71ccf271ff57
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-29 02:52:38 +0530

    profile related apis completed

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,47 @@
 const router = require("express").Router();
 const prisma = require("../db").getInstance();
-const axios = require("axios").default;
+
+// Fetch user profile
+router.get("/", async (req, res) => {
+    const user = req.user;
+    const profile = await prisma.profile.findFirstOrThrow({
+        where: {
+            id: user.id
+        },
+        select: {
+            name: true,
+            email: true,
+            avatar: true,
+            phone: true,
+            description: true
+        }
+    })
+    res.send(profile);
+})
+
+// Update user profile
+router.put("/", async (req, res) => {
+    const user = req.user;
+    const { name, avatar, phone, description } = req.body;
+    const profile = await prisma.profile.update({
+        where: {
+            id: user.id
+        },
+        data: {
+            name: name,
+            avatar: avatar,
+            phone: phone,
+            description: description
+        },
+        select: {
+            name: true,
+            email: true,
+            avatar: true,
+            phone: true,
+            description: true
+        }
+    })
+    res.send(profile);
+})
 
 module.exports = router;
