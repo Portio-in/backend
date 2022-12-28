3752d9f438741caa154883f3bc59cb40bf6dfc0f
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-29 00:53:19 +0530

    auth middleware added

---
+++ middleware.js
@@ -0,0 +1,37 @@
+const prisma = require("./db").getInstance();
+
+class Middleware {
+    /**
+     * 
+     * @param {Request} req 
+     * @param {Response} res 
+     * @param {NextFunction} next 
+     */
+    static async auth(req, res, next) {
+        try {
+            const token = req.headers.authorization;
+            if(token === null || token === undefined) throw new Error("Unauthorized");
+            const user = await prisma.apiToken.findFirstOrThrow({
+                where: {
+                    key: token
+                },
+                select: {
+                    profille: {
+                        select: {
+                            id: true,
+                            email: true
+                        }
+                    }
+                }
+            })
+            req.user = user;
+            next();
+        } catch (err) {
+            res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+    }
+}
+
+module.exports = Middleware;
