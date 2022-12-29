d0e9f99a80fb9fa0095f471d33379e2a7ec47f7c
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-29 17:25:42 +0530

    available types  for techstack and sociallink aded

--- routes/available.js
+++ routes/available.js
@@ -2,4 +2,28 @@ const router = require("express").Router();
 const prisma = require("../db").getInstance();
 const axios = require("axios").default;
 
+// Fetch all available tech stacks
+router.get("/techstack", async (req, res) => {
+    const techstacks = await prisma.techStackType.findMany({
+        select: {
+            id: true,
+            name: true,
+            icon: true
+        }
+    });
+    res.json(techstacks);
+})
+
+// Fetch all available social link types
+router.get("/sociallink", async (req, res) => {
+    const sociallinks = await prisma.socialType.findMany({
+        select: {
+            id: true,
+            type: true,
+            icon: true
+        }
+    });
+    res.json(sociallinks);
+})
+
 module.exports = router;
