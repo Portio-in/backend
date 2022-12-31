d3f95af35c0f0f72dd9853a97e656f0e705ae822
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-31 22:31:06 +0530

    api for certificate completed

--- routes/certificate.js
+++ routes/certificate.js
@@ -2,4 +2,120 @@ const router = require("express").Router();
 const prisma = require("../db").getInstance();
 const axios = require("axios").default;
 
+// Fetch all certificates
+router.get("/all", async (req, res, next) => {
+    try {
+        const certificates = await prisma.certificate.findMany({
+            where: {
+                profileId: req.user.id
+            },
+            select: {
+                id: true,
+                title: true,
+                link: true,
+                completedOn: true
+            }
+        });
+        res.status(200).json(certificates);
+    } catch (error) {
+        next(error);
+    }
+})
+
+// Add a new certificate
+router.post("/", async (req, res, next) => {
+    try {
+        const { title, link, completed_on } = req.body;
+        if(title === "" || title === undefined || title === null) res.status(400).json({ message: "Missing title" });
+        if(link === "" || link === undefined || link === null) res.status(400).json({ message: "Missing link" });
+        if(completed_on === "" || completed_on === undefined || completed_on === null) res.status(400).json({ message: "Missing completed_on" });
+
+        let completedOn = new Date(completed_on);
+        if(completedOn == "Invalid Date") res.status(400).json({ message: "Invalid completed_on" });
+
+        const certificate = await prisma.certificate.create({
+            data: {
+                title,
+                link,
+                completedOn,
+                profileId: req.user.id
+            },
+            select: {
+                id: true,
+                title: true,
+                link: true,
+                completedOn: true
+            }
+        });
+        res.status(200).json(certificate);
+    } catch (error) {
+        next(error);
+    }
+})
+
+// Update a certificate
+router.put("/:id", async (req, res, next) => {
+    try {
+        const { title, link, completed_on } = req.body;
+        if(title === "" || title === undefined || title === null) res.status(400).json({ message: "Missing title" });
+        if(link === "" || link === undefined || link === null) res.status(400).json({ message: "Missing link" });
+        if(completed_on === "" || completed_on === undefined || completed_on === null) res.status(400).json({ message: "Missing completed_on" });
+
+        let completedOn = new Date(completed_on);
+        if(completedOn == "Invalid Date") res.status(400).json({ message: "Invalid completed_on" });
+
+        // Verify if the certificate exists and belongs to the user
+        await prisma.certificate.findFirstOrThrow({
+            where: {
+                id: parseInt(req.params.id),
+                profileId: req.user.id
+            }
+        })
+
+        // Update the certificate
+        const certificate = await prisma.certificate.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: {
+                title,
+                link,
+                completedOn
+            },
+            select: {
+                id: true,
+                title: true,
+                link: true,
+                completedOn: true
+            }
+        });
+        res.status(200).json(certificate);
+    } catch (error) {
+        next(error);
+    }
+})
+
+// Delete a certificate
+router.delete("/:id", async (req, res, next) => {
+    try {
+        // Verify if the certificate exists and belongs to the user
+        await prisma.certificate.findFirstOrThrow({
+            where: {
+                id: parseInt(req.params.id),
+                profileId: req.user.id
+            }
+        })
+
+        // Delete the certificate
+        await prisma.certificate.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.status(200).json({ message: "Certificate deleted" });
+    } catch (error) {
+        next(error);
+    }
+})
+
 module.exports = router;
